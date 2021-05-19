ca087048dad541dc64d696cd561f07a7f9eb8bd2
jaimelg27 <61801163+jaimelg27@users.noreply.github.com>
2021-05-19 22:21:06 +0200

    Update save money

--- resources/php/money.php
+++ resources/php/money.php
@@ -15,20 +15,55 @@ $money=$_POST['money'];
 $selected_account=$_POST['account'];
 
 if ($selected_process=='save'){
+    $sql = "SELECT * from cuentasbancarias WHERE Account_Num='$selected_account'";
+    $resultado = $db_connection->query($sql);
+    $datos=$resultado->fetch_array();
+    if($datos['Saldo']==null){
+        $sql="UPDATE cuentasbancarias SET saldo = '0' where Account_Num='$selected_account'";
+        $db_connection->query($sql);
+    }
+    $sql2= "UPDATE cuentasbancarias SET saldo = saldo + '$money' where Account_Num='$selected_account'";
+    if ($db_connection->query($sql2)==TRUE){?>
+        <script language="javascript" type="text/javascript">
+            alert('Ha ingresado dinero correctamente.');
+            window.location = '../../user/index.php';
+        </script>
+        <?php
 
+    } else {?>
+        <script language="javascript" type="text/javascript">
+            alert('Ha habido algun problema.');
+            window.location = '../../user/money.php';
+        </script>
+        <?php
+    }
 } else if ($selected_process=='out'){
     $sql = "SELECT * from cuentasbancarias WHERE Account_Num='$selected_account'";
     $resultado = $db_connection->query($sql);
     $datos=$resultado->fetch_array();
-    if($datos['Saldo']<0){?>
+    if(($datos['Saldo']<0) or ($datos['Saldo']==null)){?>
         <script language="javascript" type="text/javascript">
-            alert('Su cuenta se encuentra en numeros rojos. No puede sacar dinero.');
+            alert('Su cuenta se encuentra en numeros rojos o no ha introducido dinero. No puede sacar dinero.');
             window.location = '../../user/money.php';
         </script>
     <?php } else {
+            $sql2= "UPDATE cuentasbancarias SET saldo = saldo - '$money' where Account_Num='$selected_account'";
+            if ($db_connection->query($sql2)==TRUE){?>
+                <script language="javascript" type="text/javascript">
+                    alert('Ha sacado dinero correctamente.');
+                    window.location = '../../user/index.php';
+                </script>
+                <?php
 
+            } else {?>
+                <script language="javascript" type="text/javascript">
+                    alert('Ha habido algun problema.');
+                    window.location = '../../user/money.php';
+                </script>
+                <?php
+            }
         }
 
     }
-}
+
 
